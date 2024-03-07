92267c921079ab77d442104d25e24708a4abaffc
namdoanx03 <dpnbro1102@gmail.com>
2024-03-07 23:38:24 +0700

    init project

---
+++ src/migrations/migration-create-schedule.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('schedules', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            currentNumber: {
+                type: Sequelize.INTEGER
+            },
+            maxNumber: {
+                type: Sequelize.INTEGER
+            },
+            date: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            timeType: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            doctorId: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('schedules');
+    }
+};
--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -3,9 +3,14 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'Namdoanx',
+      lastName: 'NamDP',
+      address:'USA',
+      gender: 1,
+      typeRole:"ROLE",
+      keyRole:"R1",
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
