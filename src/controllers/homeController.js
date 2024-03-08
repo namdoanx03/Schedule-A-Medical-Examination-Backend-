b8b0c0d6bfa1fe570071c9028bd5ff3e6a86c9d3
namdoanx03 <dpnbro1102@gmail.com>
2024-03-08 12:43:42 +0700

    init project

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from '../models/index'
+import CRUDService from '../services/CRUDService'
 
 let getHomePage = async (req, res) => {
     try {
@@ -12,7 +13,16 @@ let getHomePage = async (req, res) => {
     }
 
 }
-
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs')
+}
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body)
+    console.log(message)
+    return res.send('post crud from server')
+}
 module.exports = {
-    getHomePage: getHomePage
+    getHomePage: getHomePage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD
 };
