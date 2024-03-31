dcfb63e9550467c73f9829b5623bea4c09da2cea
namdoanx03 <dpnbro1102@gmail.com>
2024-03-31 18:30:38 +0700

    init project

--- src/server.js
+++ src/server.js
@@ -29,8 +29,10 @@ app.use(function (req, res, next) {
     next();
 });
 //config app
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit:'50mb' ,extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
--- src/services/userService.js
+++ src/services/userService.js
@@ -133,7 +133,8 @@ let createNewUser = (data) => {
                     phoneNumber: data.phonenumber,
                     gender: data.gender,
                     roleId: data.roleId,
-                    positionId: data.positionId
+                    positionId: data.positionId,
+                    image: data.avatar
                 })
                 resolve({
                     errCode: 0,
@@ -171,7 +172,7 @@ let deleteUser = (userId) => {
 let updateUserData = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.id) {
+            if (!data.id || !data.roleId || !data.positionId || !data.gender) {
                 resolve({
                     errCode: 2,
                     errMessage: 'Missing required parameters'
@@ -185,6 +186,13 @@ let updateUserData = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.gender = data.gender;
+                user.phonenumber = data.phonenumber;
+                if(data.avatar){
+                    user.image = data.avatar;
+                }
 
                 await user.save()
                 resolve({
