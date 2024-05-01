182ee3d149dcfa953aab5db0c27c86d8fd68bcfd
namdoanx03 <dpnbro1102@gmail.com>
2024-05-01 23:52:41 +0700

    init project

---
+++ src/controllers/clinicController.js
@@ -0,0 +1,19 @@
+import clinicService from '../services/clinicService'
+
+let createClinic = async (req, res) =>{
+    try {
+        let infor = await clinicService.createClinic(req.body)
+        return res.status(200).json({
+            infor
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
+module.exports ={
+    createClinic: createClinic
+}
--- src/routes/web.js
+++ src/routes/web.js
@@ -4,6 +4,8 @@ import userController from "../controllers/userController"
 import doctorController from '../controllers/doctorController'
 import patientController from '../controllers/patientController'
 import specialtyController from "../controllers/specialtyController";
+import clinicController from  "../controllers/clinicController"; 
+
 let router = express.Router()
 
 let initWebRoutes = (app) => {
@@ -37,6 +39,7 @@ let initWebRoutes = (app) => {
     router.get('/api/get-specialty', specialtyController.getAllSpecialty)
     router.get('/api/get-detail-specialty-by-id', specialtyController.getDetailSpecialtyById)
 
+    router.post('/api/create-new-clinic', clinicController.createClinic)
 
     //rest api
     return app.use("/", router)
